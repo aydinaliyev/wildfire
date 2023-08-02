4b898b0123b369a41e6dfdb03fbaf4e383db1613
aydinaliyev <76198624+aydinaliyev@users.noreply.github.com>
2023-08-03 01:09:56 +0400

    fix button issue
    button issue fixed color

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -8,12 +8,13 @@ interface CustomProps extends HTMLAttributes<HTMLDivElement> {
 }
 
 const Button: FC<CustomProps> = ({ text, startIcon, onClick, variant }) => {
+    const isOutlined = variant === 'outlined'
     return (
         <div
-            className={`flex flex-row min-w-[130px]  justify-center py-3.5 px-4 gap-[5px] bg-${
-                !!variant ? 'transparent' : '[#6E44FF]'
+            className={`flex flex-row min-w-[130px]  justify-center py-3.5 px-4 gap-[5px] ${
+                isOutlined ? 'bg-transparent' : 'bg-[#6E44FF]'
             } rounded-md shadow-button cursor-pointer
-            ${!!variant ? 'border border-[#4C4876]' : null} 
+            ${isOutlined ? 'border border-[#4C4876]' : ''} 
             `}
             onClick={onClick}
         >
